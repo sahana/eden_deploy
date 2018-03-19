5f68aaa9b2f08b29faedb13849edfb75184ce136
Fran Boon <fran@aidiq.com>
2018-03-19 20:03:52 +0000

    Bugfix

--- roles/common/files/read_hmac_key.py
+++ roles/common/files/read_hmac_key.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Assumes default appname for setup!
-input = os.path.join("/", "home", "setup", "applications", "eden", "models", "000_config.py")
-inputFile = open(input, "r")
 output = os.path.join("/", "tmp", "hmac_key")
 outputFile = open(output, "w")
-
-key = 'settings.auth.hmac_key = "'
-for line in inputFile:
-    if line.startswith(key):
-        hmac_key = line.split(key)[1].split('"')[0]
-        outputFile.write(hmac_key)
-        break
-
-inputFile.close()
+outputFile.write(settings.get_auth_hmac_key())
 outputFile.close()
