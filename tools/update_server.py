aa90fa9211ee2c9653e9f44d56e087658d513f23
Fran Boon <fran@aidiq.com>
2020-01-24 18:11:48 +0000

    update_server fix

--- tools/update_server.py
+++ tools/update_server.py
@@ -14,28 +14,28 @@ import sys
 # Parse Arguments
 # argv[0] is the script name
 try:
-    server_id = argv[1]
+    server_id = sys.argv[1]
 except IndexError:
     print("No server_id supplied")
     sys.exit(2)
 #else:
 #    print("server_id: %s" % server_id)
 try:
-    instance_id = argv[2]
+    instance_id = sys.argv[2]
 except IndexError:
     print("No instance_id supplied")
     sys.exit(2)
 #else:
 #    print("instance_id: %s" % instance_id)
 try:
-    public_ip = argv[3]
+    public_ip = sys.argv[3]
 except IndexError:
     print("No public_ip supplied")
     sys.exit(2)
 #else:
 #    print("public_ip: %s" % public_ip)
 try:
-    private_key = argv[4] # Name of file in /tmp
+    private_key = sys.argv[4] # Name of file in /tmp
 except IndexError:
     print("No private_key supplied")
     sys.exit(2)
