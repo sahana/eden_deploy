ad529e230ffb172c1af99746cb65fbc58e8e6e1c
Fran Boon <fran@aidiq.com>
2020-01-24 13:58:31 +0000

    Debug

--- tools/update_server.py
+++ tools/update_server.py
@@ -5,6 +5,9 @@
 #
 # Run as:
 #   python web2py.py -S eden -M -R applications/eden/private/eden_deploy/tools/update_server.py -A server_id instance_id public_ip private_key
+#
+# NB A generic way to find out the Public IP is https://docs.ansible.com/ansible/latest/modules/ipify_facts_module.html
+#
 
 import sys
 
@@ -15,28 +18,35 @@ def main(argv):
     except IndexError:
         print("No server_id supplied")
         sys.exit(2)
+    else:
+        print("server_id: %s" % server_id)
     try:
         instance_id = argv[1]
     except IndexError:
         print("No instance_id supplied")
         sys.exit(2)
+    else:
+        print("instance_id: %s" % instance_id)
     try:
         public_ip = argv[2]
     except IndexError:
         print("No public_ip supplied")
         sys.exit(2)
+    else:
+        print("public_ip: %s" % public_ip)
     try:
         private_key = argv[3] # Name of file in /tmp
     except IndexError:
         print("No private_key supplied")
         sys.exit(2)
+    else:
+        print("private_key: %s" % private_key)
 
     # Update Server record
     table = s3db.setup_server
-
-    # Upload SSH Private Key to the Server record
     private_key_path = os.path.join("/", "tmp", private_key)
     field = table.private_key
+    newfilename = None
     with open(private_key_path, "r") as private_key_file:
         newfilename = field.store(private_key_file,
                                   "%s.pem" % private_key,
@@ -46,8 +56,8 @@ def main(argv):
                                      )
 
     # Update AWS Server record with the Instance ID
-    table = s3db.setup_aws_server
-    db(table.server_id == server_id).update(instance_id = instance_id)
+    atable = s3db.setup_aws_server
+    db(atable.server_id == server_id).update(instance_id = instance_id)
 
     db.commit()
 
