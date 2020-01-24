65eb83a88eb593ea7d8eb80d46798b6bf57f9db9
Fran Boon <fran@aidiq.com>
2020-01-24 12:26:04 +0000

    update_server improved

--- tools/update_server.py
+++ tools/update_server.py
@@ -6,38 +6,51 @@
 # Run as:
 #   python web2py.py -S eden -M -R applications/eden/private/eden_deploy/tools/update_server.py -A server_id instance_id public_ip private_key
 
+import sys
+
 def main(argv):
     # Parse Arguments
-    server_id = argv[0]
-    instance_id = argv[1]
-    public_ip = argv[2]
-    private_key = argv[3] # Name of file in /tmp
-
-    # Debug to stdout
-    print("server_id: " % server_id)
-    print("instance_id: " % instance_id)
-    print("public_ip: " % public_ip)
-    print("private_key: " % private_key)
+    try:
+        server_id = argv[0]
+    except IndexError:
+        print("No server_id supplied")
+        sys.exit(2)
+    try:
+        instance_id = argv[1]
+    except IndexError:
+        print("No instance_id supplied")
+        sys.exit(2)
+    try:
+        public_ip = argv[2]
+    except IndexError:
+        print("No public_ip supplied")
+        sys.exit(2)
+    try:
+        private_key = argv[3] # Name of file in /tmp
+    except IndexError:
+        print("No private_key supplied")
+        sys.exit(2)
 
     # Update Server record
     table = s3db.setup_server
 
-    if private_key is not None:
-        # Upload SSH Private Key to the Server record
-        private_key_path = os.path.join("/", "tmp", private_key)
-        field = table.private_key
-        with open(private_key_path, "r") as private_key_file:
-            newfilename = field.store(private_key_file,
-                                      "%s.pem" % private_key,
-                                      field.uploadfolder)
-        db(table.id == server_id).update(host_ip = public_ip,
-                                         private_key = newfilename,
-                                         )
-    else:
-        db(table.id == server_id).update(host_ip = public_ip)
+    # Upload SSH Private Key to the Server record
+    private_key_path = os.path.join("/", "tmp", private_key)
+    field = table.private_key
+    with open(private_key_path, "r") as private_key_file:
+        newfilename = field.store(private_key_file,
+                                  "%s.pem" % private_key,
+                                  field.uploadfolder)
+    db(table.id == server_id).update(host_ip = public_ip,
+                                     private_key = newfilename,
+                                     )
 
     # Update AWS Server record with the Instance ID
     table = s3db.setup_aws_server
     db(table.server_id == server_id).update(instance_id = instance_id)
 
     db.commit()
+
+if __name__ == "__main__":
+
+    sys.exit(main(sys.argv[1:]))
