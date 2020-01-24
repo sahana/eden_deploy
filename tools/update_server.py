22479825a64168e6f5c4f6360723d30bc4af214f
Fran Boon <fran@aidiq.com>
2020-01-24 18:00:33 +0000

    Fix update_server

--- tools/update_server.py
+++ tools/update_server.py
@@ -11,56 +11,52 @@
 
 import sys
 
-def main(argv):
-    # Parse Arguments
-    try:
-        server_id = argv[0]
-    except IndexError:
-        print("No server_id supplied")
-        sys.exit(2)
-    else:
-        print("server_id: %s" % server_id)
-    try:
-        instance_id = argv[1]
-    except IndexError:
-        print("No instance_id supplied")
-        sys.exit(2)
-    else:
-        print("instance_id: %s" % instance_id)
-    try:
-        public_ip = argv[2]
-    except IndexError:
-        print("No public_ip supplied")
-        sys.exit(2)
-    else:
-        print("public_ip: %s" % public_ip)
-    try:
-        private_key = argv[3] # Name of file in /tmp
-    except IndexError:
-        print("No private_key supplied")
-        sys.exit(2)
-    else:
-        print("private_key: %s" % private_key)
+# Parse Arguments
+# argv[0] is the script name
+try:
+    server_id = argv[1]
+except IndexError:
+    print("No server_id supplied")
+    sys.exit(2)
+#else:
+#    print("server_id: %s" % server_id)
+try:
+    instance_id = argv[2]
+except IndexError:
+    print("No instance_id supplied")
+    sys.exit(2)
+#else:
+#    print("instance_id: %s" % instance_id)
+try:
+    public_ip = argv[3]
+except IndexError:
+    print("No public_ip supplied")
+    sys.exit(2)
+#else:
+#    print("public_ip: %s" % public_ip)
+try:
+    private_key = argv[4] # Name of file in /tmp
+except IndexError:
+    print("No private_key supplied")
+    sys.exit(2)
+#else:
+#    print("private_key: %s" % private_key)
 
-    # Update Server record
-    table = s3db.setup_server
-    private_key_path = os.path.join("/", "tmp", private_key)
-    field = table.private_key
-    newfilename = None
-    with open(private_key_path, "r") as private_key_file:
-        newfilename = field.store(private_key_file,
-                                  "%s.pem" % private_key,
-                                  field.uploadfolder)
-    db(table.id == server_id).update(host_ip = public_ip,
-                                     private_key = newfilename,
-                                     )
+# Update Server record
+table = s3db.setup_server
+private_key_path = os.path.join("/", "tmp", private_key)
+field = table.private_key
+newfilename = None
+with open(private_key_path, "r") as private_key_file:
+    newfilename = field.store(private_key_file,
+                              "%s.pem" % private_key,
+                              field.uploadfolder)
+db(table.id == server_id).update(host_ip = public_ip,
+                                 private_key = newfilename,
+                                 )
 
-    # Update AWS Server record with the Instance ID
-    atable = s3db.setup_aws_server
-    db(atable.server_id == server_id).update(instance_id = instance_id)
+# Update AWS Server record with the Instance ID
+atable = s3db.setup_aws_server
+db(atable.server_id == server_id).update(instance_id = instance_id)
 
-    db.commit()
-
-if __name__ == "__main__":
-
-    sys.exit(main(sys.argv[1:]))
+db.commit()
