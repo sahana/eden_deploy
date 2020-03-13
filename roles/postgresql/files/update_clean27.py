3f2f2250997532a1aacb9ff4125df8a7acdd0d08
Fran Boon <fran@aidiq.com>
2020-03-13 11:53:04 +0000

    update_clean support for Py3

--- roles/postgresql/files/update_clean27.py
+++ roles/postgresql/files/update_clean27.py
@@ -1,10 +1,10 @@
 import hashlib
 db_string = settings.get_database_string()[1]
-prod_table_string = hashlib.md5(db_string.encode("utf-8")).hexdigest()
+prod_table_string = hashlib.md5(db_string).hexdigest()
 settings._db_params = None
 settings.database.database = "sahana-test"
 db_string = settings.get_database_string()[1]
-test_table_string = hashlib.md5(db_string.encode("utf-8")).hexdigest()
+test_table_string = hashlib.md5(db_string).hexdigest()
 File = open("/usr/local/bin/clean", "r")
 file = File.readlines()
 File.close()
