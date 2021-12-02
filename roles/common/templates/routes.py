32f31afd674d7d9204992cf55685b2ec5a132302
Fran Boon <fran@aidiq.com>
2021-12-02 17:00:02 +0000

    Exclude 405/409 errors from routes_onerror

--- roles/common/templates/routes.py
+++ roles/common/templates/routes.py
@@ -5,6 +5,8 @@ default_function = 'index'
 routes_onerror = [
         ('{{ appname }}/400', '!'),
         ('{{ appname }}/401', '!'),
+        ('{{ appname }}/405', '!'),
+        ('{{ appname }}/409', '!'),
         ('{{ appname }}/509', '!'),
         ('{{ appname }}/*', '/{{ appname }}/errors/index'),
         ('*/*', '/{{ appname }}/errors/index'),
